2fa0c48aaba8dc72579907fa66814f7a0fbc7391
Mario Bodom <mariotecladista666@gmail.com>
2022-12-09 19:29:01 +0100

    Arreglamos la fecha de registro, añadimos correo y botones de aceptar y cancelar

--- app/src/main/java/com/example/tfg/RegisterWindow.java
+++ app/src/main/java/com/example/tfg/RegisterWindow.java
@@ -1,13 +1,52 @@
 package com.example.tfg;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterWindow extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class RegisterWindow extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    TextView fechaText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_window);
+        fechaText = findViewById(R.id.editTextDOB);
+    }
+
+    public void chooseDate(View view){
+        DialogFragment datePicker = new DatePickerFragment();
+        datePicker.show(getSupportFragmentManager(), String.valueOf(R.string.dob));
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, day);
+        String dob = DateFormat.getDateInstance().format(c.getTime());
+        fechaText.setText(dob);
+    }
+
+    public void accept(View view){
+        Toast.makeText(this, "El usuario ha sido creado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancel(View view){
+        Intent cancelIntent = new Intent(this, MainActivity.class);
+        startActivity(cancelIntent);
+        finishAfterTransition();
     }
 }
