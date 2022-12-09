6629f28756457b52996ac29a4e8f8ed3e315d57f
Mario Bodom <mariotecladista666@gmail.com>
2022-12-09 17:39:45 +0100

    Se termina la primera version del loginProcess()

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -6,19 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String userNameAdmin = "Admin";
     String passwordAdmin = "Admin.123";
     EditText userNameField;
-    TextView passwordField;
+    EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         userNameField = findViewById(R.id.userField);
+        passwordField = findViewById(R.id.passField);
 
     }
 
@@ -29,10 +31,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginProcess(View view){
         Intent intentLog = new Intent(this, Profile.class);
-        boolean userName =
-        if ()
-        startActivity(intentLog);
-        finishAfterTransition();
+        boolean userNameCorrect = String.valueOf(userNameField.getText()).equals(userNameAdmin);
+        boolean passwordCorrect = String.valueOf(passwordField.getText()).equals(passwordAdmin);
+        if (userNameCorrect && passwordCorrect){
+            startActivity(intentLog);
+            finishAfterTransition();
+        }
+        if ((!userNameCorrect) && (!passwordCorrect)){
+            Toast.makeText(this, R.string.incorrectUserPass, Toast.LENGTH_SHORT).show();
+        }else if (!userNameCorrect){
+            Toast.makeText(this, R.string.incorrectUser, Toast.LENGTH_SHORT).show();
+        }else if (!passwordCorrect){
+            Toast.makeText(this, R.string.incorrectPass, Toast.LENGTH_SHORT).show();
+        }
     }
     public void forgotPass(View view){
         Intent intentFPass = new Intent(this, loginv2.class);
